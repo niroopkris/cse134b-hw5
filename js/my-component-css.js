97369b9b7c336a9d6db56bdcd2cf48612b842bb5
niroopkris <niroopkris@gmail.com>
2025-03-15 19:45:02 -0700

    added basic html template for project-cards, plus its corresponding JS code. also updated navigation header to include this page

---
+++ js/my-component-css.js
@@ -0,0 +1,30 @@
+//CSS for our custom component, no need to do anything here!
+export function getMyComponentCSS() {
+    return `
+                .project-card {
+                    background: white;
+                    border-radius: 10px;
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                    overflow: hidden;
+                    max-width: 300px;
+                    text-align: left;
+                }
+
+                .project-card img {
+                    width: 100%;
+                    height: auto;
+                    display: block;
+                }
+
+                hgroup .desc {
+                    font-size: 0.9rem;
+                    color: #888;
+                    margin: 10px;
+                }
+
+                hgroup .proj-name {
+                    font-size: 1.2rem;
+                    margin: 10px;
+                }
+                `;
+}
---
+++ js/my-component.js
@@ -0,0 +1,53 @@
+//TODO 3: import CSS function getMyComponentCSS()
+import { getMyComponentCSS } from './my-component-css.js';
+
+class MyProject extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        // HTML and inline style as template literals for easy modification
+        const style = document.createElement('style');
+        // TODO 4: Uncomment this once you successfully import the CSS function
+         style.textContent = getMyComponentCSS();
+
+        //change details to match a project (ex: name, short description, link to further reading)
+        const projName = this.getAttribute('proj-name') || 'Unknown Name';
+        const desc = this.getAttribute('desc') || 'Unknown Description';
+        const aLink = this.getAttribute('a-link') || 'Unknown Link';
+        const imageUrl = this.getAttribute('image-url') || '';
+
+        // Template for component content
+        //TODO 1: set up the innerHTML of the component as follows:
+        // <div class="simple-card">
+        //         <hgroup>
+        //         <h2 class="projName">${projName}</h2>
+        //         <p class="desc">${desc}</p>
+        //         <a class="desc" href=${aLink}>Further Reading</a>
+        //         </hgroup>
+        //         <img src="${imageUrl}" alt="${projName} Website">
+        //     </div>
+        // also have somewhere to append style to
+        let myDiv = document.createElement('div');
+        myDiv.innerHTML = '<hgroup> <h2 class="projName">${projName}</h2> <p class="desc">${desc}</p> <a class="desc" href=${aLink}>Further Reading</a> </hgroup> <img src="${imageUrl}" alt="${projName} Website">' 
+
+        //TODO 2: Uncomment this once you have the HTML set up
+        this.querySelector('.project-card').addEventListener('click', () => this.doSomething());
+
+    }
+    doSomething() {
+        //TODO 6: set up const event as a new CustomEvent, which will bubble up to the DOM root
+        // 02-event-delegation.html may help with this (see line 58) but you won't need message or detail
+        //dont forget to add this.dispatchEvent(event) at the end
+        const event = new CustomEvent('custom-message', {
+            bubbles: true
+        });
+        this.dispatchEvent(event);
+        //this.dispatchEvent(event)
+    }
+}
+
+// Define the custom element
+console.log("custom element defined");
+customElements.define('my-project', MyProject);
